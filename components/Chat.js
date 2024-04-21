bc27c89ba5fe165dde9d5d53a33ef4c1c20e6385
Mezekr W <mezi.edu@tuta.io>
2024-04-21 10:44:41 +0200

    implement a message bubble for sending messages

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import {
 	Image,
+	KeyboardAvoidingView,
+	Platform,
 	ScrollView,
 	StyleSheet,
 	Text,
@@ -8,94 +10,91 @@ import {
 	TouchableOpacity,
 	View,
 } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import sendMessage from '../assets/sendMessage.png';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
+	// Name to display on the States bar and
+	// Chat Background Color vuleus from the start sceen
 	const { name, chatBackgroundColor } = route.params;
 
-	const [text, setText] = useState('');
-	const [message, setMessage] = useState('');
+	// State used to hold the message Send
+	const [messages, setMessages] = useState([]);
 
+	// Displays massages at the start of the application
 	useEffect(() => {
 		navigation.setOptions({ title: name });
+		setMessages([
+			{
+				_id: 1,
+				text: 'Hello developer',
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: 'React Native',
+					avatar: 'https://placeimg.com/140/140/any',
+				},
+			},
+			{
+				_id: 2,
+				text: 'This is a system message',
+				createdAt: new Date(),
+				system: true,
+			},
+		]);
 	}, []);
 
-	const handlePress = () => {
-		setMessage(text);
-		setText('');
+	const onSend = (newMessages) => {
+		setMessages((previousMessages) =>
+			GiftedChat.append(previousMessages, newMessages)
+		);
+	};
+
+	// Change the bubble background colour based on the chat background colour
+	const bubbleBgColor =
+		chatBackgroundColor === '#090C08' ? '#707070' : '#000';
+
+	// Customize the GiftedChat message bubble component
+	const renderBubble = (props) => {
+		return (
+			<Bubble
+				{...props}
+				wrapperStyle={{
+					right: {
+						backgroundColor: bubbleBgColor,
+					},
+					left: {
+						backgroundColor: '#FFF',
+					},
+				}}
+			/>
+		);
 	};
 
 	return (
-		// <SafeAreaView>
-		<ScrollView style={{ backgroundColor: chatBackgroundColor }}>
-			<View style={styles.chatContainer}>
-				<Text style={styles.textDisplay}>{message}</Text>
-
-				<View style={styles.inputContainer}>
-					<TextInput
-						style={styles.chatTextInput}
-						value={text}
-						onChangeText={setText}
-					/>
-					<TouchableOpacity
-						style={styles.button}
-						onPress={handlePress}
-					>
-						<Image
-							style={styles.sendMessageIcon}
-							source={sendMessage}
-							resizeMode="contain"
-						/>
-					</TouchableOpacity>
-				</View>
-			</View>
-		</ScrollView>
-		// </SafeAreaView>
+		<View
+			style={[styles.container, { backgroundColor: chatBackgroundColor }]}
+		>
+			<GiftedChat
+				messages={messages}
+				renderBubble={renderBubble}
+				onSend={(messages) => onSend(messages)}
+				user={{
+					_id: 1,
+				}}
+			/>
+			{Platform.OS === 'android' ? (
+				<KeyboardAvoidingView behavior="height" />
+			) : (
+				<KeyboardAvoidingView behavior="padding" />
+			)}
+		</View>
 	);
 };
 
 export default Chat;
 
 const styles = StyleSheet.create({
-	chatContainer: {
+	container: {
 		flex: 1,
-		justifyContent: 'flex-end',
-		alignItems: 'flex-end',
-		padding: 10,
-	},
-	textDisplay: {
-		flex: 0.8,
-		height: 100,
-		lineHeight: 30,
-	},
-	inputContainer: {
-		flex: 0.2,
-		flexDirection: 'row',
-		borderWidth: 1,
-		borderColor: '#b4b4b4',
-		alignItems: 'center',
-		gap: 5,
-	},
-	chatTextInput: {
-		fontSize: 12,
-		fontWeight: '300',
-		color: '#757083',
-		opacity: 50,
-		width: '88%',
-		height: 50,
-		padding: 4,
-		borderWidth: 1,
-		borderColor: '#b4b4b4',
-	},
-	sendMessageIcon: {
-		width: 40,
-		height: 40,
-		padding: 5,
-	},
-	button: {
-		alignItems: 'center',
-		width: '10%',
-		height: 40,
 	},
 });
