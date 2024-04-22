086989cdb88f6746f6b99feeeadeafa90dcca148
Mezekr W <mezi.edu@tuta.io>
2024-04-22 08:47:03 +0200

    add Firestore config and reference to its service

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import { StyleSheet, Text, View } from 'react-native';
 import Chat from './components/Chat';
 import Start from './components/Start';
 
 const App = () => {
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: 'AIzaSyAvoMwnLUNqchhzI02kLDtH_YE0nuUaucs',
+		authDomain: 'mezichatapp.firebaseapp.com',
+		projectId: 'mezichatapp',
+		storageBucket: 'mezichatapp.appspot.com',
+		messagingSenderId: '703795610972',
+		appId: '1:703795610972:web:da0c4c89576062459b2d99',
+	};
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+
+	// Initialize Cloud Firestore and get a reference to the service
+	const db = getFirestore(app);
+
 	const Stack = createNativeStackNavigator();
 	return (
 		<NavigationContainer>
